fc35ad8c8e372b7a56f0d5af82d4f36294c237fe
azzansm <89733998+azzansm@users.noreply.github.com>
2023-01-12 22:24:09 +0700

    Delete mylib directory

--- mylib/mylib.c
+++
@@ -1,7 +0,0 @@
-void states(int s0, int s1, int b0, int b1, int* n0, int* n1, int* o0, int* o1) {
-    // from kmap
-    *n0 = ((!s1) & b0)| (s0 & b1);
-    *n1= ((!s0) & (!s1) & b1) | (s1 & (!b0) & (!b1));
-    *o0 = s0;
-    *o1 = s1;
-}
--- mylib/mylib.h
+++
@@ -1,1 +0,0 @@
-void states(int s0, int s1, int b0, int b1, int* n0, int* n1, int* o0, int* o1) ;
